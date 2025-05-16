5a9ad71444b5592f37c9649d2c53af4463363c43
Maksym Habinskyi <mgabin348@gmail.com>
2025-05-17 00:12:26 +0200

    commands are loaded into the bot

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv';
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Collection,
+  MessageFlags,
+} from "discord.js";
+import dotenv from "dotenv";
+import fs from "node:fs";
+import path from "node:path";
 
 dotenv.config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildVoiceStates
-    ],
-})
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
 
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(folderPath);
+
+for (const folder of commandFiles) {
+  const commandsPath = path.join(folderPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 client.once(Events.ClientReady, () => {
-    console.log('Ready!, Logged in as ' + client.user.tag); 
+  console.log("Ready!, Logged in as " + client.user.tag);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(`Error executing ${interaction.commandName}:`, error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
-
